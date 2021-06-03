8e6bc94cde689ad32dacf7170dc9cdd131ef4bd7
Hieu Nghia Vuong <wechicken456@Hieus-MacBook-Pro.local>
2021-06-04 09:54:28 +1000

    Added new solutions

---
+++ 1443B.cpp
@@ -0,0 +1,31 @@
+
+// https://codeforces.com/problemset/problem/1443/B
+
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<map>
+#include<iterator>
+#include<algorithm>
+#include<math.h>
+#include<cmath>
+
+typedef unsigned long long ll;
+
+using namespace std;
+
+void solve(){
+	int a, b;
+	cin >> a >> b;
+	string bombs;
+	cin >> bombs;
+	cout << bombs.count('1') << "\n";
+}
+
+int main(){
+	int T;
+	cin >> T;
+	while (T--){
+		solve();
+	}
+}
