24fedafcfa6fed7c1bd3d64783cab382c6d0531d
Hieu Nghia Vuong <wechicken456@Hieus-MBP.modem>
2021-06-04 17:35:12 +1000

    Added new solutions

--- 1133D.cpp
+++ 1133D.cpp
@@ -10,21 +10,19 @@ void solve(){
 	cin >> n;
 	vector<long long>a(n), b(n);
 	for (int i = 0 ; i < n; i++){
-		scanf("%lld", &a[i]);
+		cin >> a[i];
 	}
 	for (int i = 0 ; i < n; i++){
-		scanf("%lld", &b[i]);
+		cin >> b[i];
 	}
 	map<double,long>d;
 	for (int i = 0 ; i < n; i++){
 		if (a[i] == 0) continue;
 		double temp = (double)-b[i]/a[i];
-		//cout << temp << " ";
 		d[temp]++;
 	}
 	int answer = 0;
 	for (auto &i : d){
-		//cout << i.first << " " << i.second << "\n";
 		if (i.second > answer) answer = i.second;
 	}
 	cout << answer << "\n";
