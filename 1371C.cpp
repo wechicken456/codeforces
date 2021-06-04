608277ea17ec6b318850132d7179fd999b32db0c
Hieu Nghia Vuong <wechicken456@Hieus-MacBook-Pro.local>
2021-06-04 14:46:07 +1000

    Added new solutions

--- 1371C.cpp
+++ 1371C.cpp
@@ -26,7 +26,7 @@ void solve(){
 	for (int i = 0; i < b.size()-1; i++){
 		cout << b[i] << " " << b[i+1] << " " << 2*k << "\n";
 		int diff = b[i+1] - b[i] - 1;
-		if (diff > 2*k) {
+		if (diff >= 2*k) {
 			int m = (diff + 1) / k;
 			//cout << "m: " << m << "\n";
 			while (m*k + m -1 > diff) m--;
