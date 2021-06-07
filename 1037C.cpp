da323742f4953a3a2555846affca8fde84295abb
Hieu Nghia Vuong <wechicken456@Hieus-MBP.modem>
2021-06-07 17:46:36 +1000

    Added new solutions

--- 1037C.cpp
+++ 1037C.cpp
@@ -12,27 +12,32 @@ typedef unsigned long long ll;
 using namespace std;
 
 void solve(){
-	int n;
+	ll n;
 	cin >> n;
 	char *a = (char*)malloc(n), *b = (char*)malloc(n);
 	scanf("%s", a);
 	scanf("%s", b);
-	vector<int> diff;
+	vector<ll> diff;
 	for (int i = 0 ; i < n; i++){
 		if (a[i] != b[i]) {
 			diff.push_back(i);
 		}
 	}
-	int answer = 0 ;
+	ll answer = 0 ;
 	if (diff.size() == 1) answer++;
-	else
-	for (int i = 1 ; i < diff.size(); i++){
-		if (diff[i] - diff[i-1] == 1){
-			answer++;
-			i++;
-		}
-		else {
-			answer++;
+	else {
+		ll diff_size = diff.size();
+		for (ll i = 0 ; i < diff_size; i++){
+			if (i == diff_size -1 ) {
+				answer++;
+			}
+			if (diff[i+1] - diff[i] == 1 && b[i+1] != b[i]){
+				answer++;
+				i++;
+			}
+			else {
+				answer++;
+			}
 		}
 	}
 	cout << answer << "\n";
