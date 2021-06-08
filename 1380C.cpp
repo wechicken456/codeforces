ded42defe45e12a791597a0588cbc79292ffda58
Hieu Nghia Vuong <wechicken456@Hieus-MacBook-Pro.local>
2021-06-08 10:26:06 +1000

    Added new solutions

--- 1380C.cpp
+++ 1380C.cpp
@@ -35,20 +35,20 @@ void solve(){
 	
 	ll rem = 0;
 	map<ll, ll>::iterator it = teams.end();
-	/*it--;
+	it = prev(it, 1);
 	temp = (ll)it->second / it->first;
 	rem = it->second - it->first * temp; 
 	answer += temp;
-	it--;
+	it = prev(it, 1);
 
-	for (; it != teams.begin() || it == teams.begin(); it--){
+	for (; it != teams.begin() || it == teams.begin(); it = prev(it, 1)){
 		it->second += rem;
 		temp = (ll)it->second / it->first;
 		rem = it->second - it->first * temp;
 		answer += temp;
 	}
 	cout << answer << "\n";
-	*/
+	
 }
 
 int main(){
