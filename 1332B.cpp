749dc3599317908b91a4ff30449fea32c49a9a67
Hieu Nghia Vuong <wechicken456@Hieus-MBP.modem>
2021-06-12 14:41:55 +1000

    Added new solutions

---
+++ 1332B.cpp
@@ -0,0 +1,69 @@
+
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<map>
+#include<iterator>
+#include<algorithm>
+#include<math.h>
+#include<cmath>
+#include<numeric>
+
+typedef  long long ll;
+
+using namespace std;
+
+void solve(){
+	int n;
+	cin >> n;
+	vector<int>a(n);
+	vector<int>color(n);
+	cin >> a[0];
+	color[0] = 1;
+	map<int, vector<int>>m;
+	m[1].push_back(a[0]);
+	bool check;
+
+	int count = 1;
+
+	for (int i = 1; i < n; i++){
+		cin >> a[i];
+		for (auto&x : m) {
+			check =false;
+			for (int j = 0 ; j < x.second.size();j ++){
+				if (gcd(a[i], x.second[j]) == 1){
+					check = true;
+					break;
+				}
+			}
+			if (check == false) {
+				color[i] = x.first;
+				x.second.push_back(a[i]);
+				break;
+			}
+		}
+		if (check == true){
+			m[++count].push_back(a[i]);
+			color[i] = count;
+		}
+	}
+
+	cout << count << "\n";
+	for (int i = 0 ; i < n; i++){
+		cout << color[i] << " ";
+	}
+	cout << "\n";
+}
+
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int T;
+	cin >> T;
+	while (T--){
+		solve();
+	}
+}
+
+
