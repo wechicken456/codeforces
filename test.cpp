6061b2510f58bc782bdde214eea0ca797672de13
Hieu Nghia Vuong <wechicken456@Hieus-MBP.modem>
2021-06-12 14:52:12 +1000

    Added new solutions

--- test.cpp
+++ test.cpp
@@ -1,4 +1,4 @@
-
+#include<numeric>
 #include<vector>
 #include<iostream>
 #include<cmath>
@@ -15,10 +15,14 @@ vector<int> factors(int n) {
 }
 
 int main(){
-	vector<int>arr = factors(12);
+	//vector<int>arr = factors(12);
 	/*for (int i = 0 ; i < arr.size(); i++){
 		cout << arr[i] << "\n";
 	}*/
-	cout << (long long)7/4 << "\n";
+	//cout << (long long)7/4 << "\n";
+	int arr[] = {437 ,519 ,865 ,808 ,909 ,391 ,194 ,291 ,237 ,395 ,323 ,365 ,511 ,497 ,781 ,737 ,871 ,559 ,731 ,697 ,779 ,841}
+	for (int i = 0 ; i < arr.size(); i++){
+		cout << gcd(961, arr[i]) << " ";
+	}
 }
 
