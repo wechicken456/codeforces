8cd0a8462fd4e1d3212790ff4005330960d5db33
Hieu Nghia Vuong <wechicken456@Hieus-MacBook-Pro.local>
2021-06-18 12:01:22 +1000

    Added new solutions

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 
 file_to_compile=$1
 output=${file_to_compile/.cpp}
-g++ -o $output $file_to_compile
+g++-11 -o $output $file_to_compile
 git add .
 git commit -am "Added new solutions"
 git pull https://github.com/wechicken456/codeforces.git
