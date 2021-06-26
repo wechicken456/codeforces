c81ba99e3a91c4f867c37bd787a33e1726dc430e
Hieu Nghia Vuong <wechicken456@Hieus-MBP.modem>
2021-06-26 13:01:35 +1000

    Added new solutions

--- 1538D.cpp
+++ 1538D.cpp
@@ -4,7 +4,6 @@ using namespace std;
 #define ll long long
 
 int count_primes(ll a){
-	if (a == 1) return 0;
 	int count = 0 ;
 	for (int p  = 2; p*p <= a; p++){
 		while (a % p == 0) {
@@ -12,7 +11,7 @@ int count_primes(ll a){
 			count++;
 		}
 	}
-	if (count != 1) {
+	if (a != 1) {
 		count++;
 	}
 	return count;
@@ -21,7 +20,6 @@ int count_primes(ll a){
 bool solve(){
 	ll a,b,k;
 	cin >> a >> b >> k;
-
 	if (k == 1) {
 		return (a != b && (a % b == 0 || b % a == 0));
 	}
