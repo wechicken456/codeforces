31c65f4e1acae1e452e6bcdec971e32f5e715f57
Hieu Nghia Vuong <wechicken456@Hieus-MBP.modem>
2021-07-17 11:06:41 +1000

    Added new solutions

---
+++ 1203E.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<stdio.h>
+#include<vector>
+#include<map>
+#include<iterator>
+#include<algorithm>
+#include<math.h>
+#include<cmath>
+#include<fstream>
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+
+using namespace std;
+
+ull power(ll x, ull y) {
+    if (y == 0)
+        return 1;
+    else if (y % 2 == 0)
+        return power(x, y / 2) * power(x, y / 2);
+    else
+        return x * power(x, y / 2) * power(x, y / 2);
+}
+ 
+ll my_ceil(ll a, ll b) {
+	return (a/b) + ( (a%b)!=0 );
+}
+
+void solve(){
+	int n;
+	set<int>s;
+	scanf("%d", &n);
+	for (int i = 0, x; i < n; i++){
+		scanf("%d", &x);
+		s.insert(x);
+		s.insert(x + 1)
+		if (x - 1 > 0) s.insert( x- 1);
+	}
+	printf("%d", s.size());
+}
+
+int main(){
+	solve();
+}
+
+
--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 
 file_to_compile=$1
 output=${file_to_compile/.cpp}
-g++-11 -o $output $file_to_compile
+g++-11 -std=c++17 -Wshadow -Wall -o $output $file_to_compile -O2 -Wno-unused-result
 git add .
 git commit -am "Added new solutions"
 git pull https://github.com/wechicken456/codeforces.git
