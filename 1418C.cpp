b388cfb3ffb5f09baf1814e58915a162c83cd6a1
Hieu Nghia Vuong <wechicken456@Hieus-MBP.modem>
2021-07-19 22:00:35 +1000

    Added new solutions

--- 1418C.cpp
+++ 1418C.cpp
@@ -39,7 +39,7 @@ void solve(){
 	for (int i = 1; i <=  n ; i++){
 		scanf("%d", &a[i]);
 	}	
-	dp[1][0] = (a[1] == 1);
+	dp[1][0] = (a[1] == 1) ? 1 : 0;
 	for (int i = 2; i <= n; i++){
 		int cur = (a[i] == 1);
 		dp[i][0] = min(dp[i-1][0] + cur, dp[i-2][1]);
