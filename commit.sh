3bb50a6922af3fa91f34229f24bfd2a5cc4186a7
Hieu Nghia Vuong <wechicken456@Hieus-MacBook-Pro.local>
2022-05-18 00:15:02 +0700

    Added new solutions

--- commit.sh
+++ commit.sh
@@ -3,6 +3,7 @@
 file_to_compile=$1
 output=${file_to_compile/.cpp}
 g++-11 -std=c++17 -Wshadow -Wall -o $output $file_to_compile -Ofast -Wno-unused-result
+git remote set-url origin https://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@github.com/wechicken456/codeforces.git/
 git add .
 git commit -am "Added new solutions"
 git pull https://github.com/wechicken456/codeforces.git
